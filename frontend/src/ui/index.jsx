2b126de25804c0cd6d6dab6ac964da9fb44509e0
DeDeimos <rodion.tolkachev.03@inbox.ru>
2023-05-21 02:30:54 +0300

    ui

--- frontend/src/ui/index.jsx
+++ frontend/src/ui/index.jsx
@@ -5,5 +5,6 @@ import { Search } from "./Search";
 import { SendButton } from "./SendButton";
 import { ChatInput } from "./ChatInput";
 import { AddChatButton } from "./AddChatButton";
+import { GenderSelect } from "./GenderSelect";
 
-export {PhoneInput, SubmitButton, BackButton, Search, SendButton, ChatInput, AddChatButton, };
+export {PhoneInput, SubmitButton, BackButton, Search, SendButton, ChatInput, AddChatButton, GenderSelect, };
