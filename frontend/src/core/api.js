3648ad84045dc862b4f073de83a967233fb8a4d7
cry1s <cry1s@ya.ru>
2023-05-20 11:42:42 +0300

    hi

---
+++ frontend/src/core/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8082/',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// return string
+export async function getHi() {
+    const response = await api.get('/hi');
+    return response.data;
+}
