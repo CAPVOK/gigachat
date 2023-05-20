5a45f3423528d5383741e61ecfdddaea89ab1dfa
CAPVOK <capvok@yandex.ru>
2023-05-20 12:49:49 +0300

    Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import slice from './core/slice.js'
+
+import './index.css'
 
 const store = configureStore({
   reducer: slice,
